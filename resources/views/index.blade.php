05c9dd6c3ce1eaa971a22a1f6f3576aa696da499
danukaji-M <danukajihansanath0408@gmail.com>
2023-12-24 18:38:14 +0530

    2nd_push

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,13 @@
 @extends('leyouts.app')
 
-@section('title' , 'test1')
+@section('title', 'test1')
 @section('content')
     <div class=" cotainer-fluid">
         <div class="row">
             <div class="col-12">
-                <h1 class="text-bold" >
-                    @if($name != null)
-                        hello, {{$name}}
+                <h1 class="text-bold">
+                    @if ($name != null)
+                        hello, {{ $name }}
                     @else
                         hello, user
                     @endif
@@ -17,3 +17,14 @@
         </div>
     </div>
 @endsection
+@push('styles')
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/styles.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+@endpush
+@push('scripts')
+    <script src="{{ asset('js/script.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+@endpush
